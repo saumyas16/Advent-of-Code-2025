013f48317a445effc2a26e2987e3dfa177dd4256
Saumya Singh <49658856+saumyas16@users.noreply.github.com>
2025-12-05 13:13:53 +0100

    Day 4 codes (AofC)

---
+++ forklift2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+int forklifting(std::vector<std::string> sequence) {
+    int sum = 0;
+    bool change = true;
+    while(change){
+        change = false;
+        for(int i=0;i<sequence.size();i++){
+            for(int j=0;j<sequence[i].size();j++){
+                if(sequence[i][j]=='.') continue;
+                int count = 0;
+                if(i-1>=0&&j-1>=0){
+                    if(sequence[i-1][j-1]=='@') count++;
+                    if(sequence[i-1][j]=='@') count++;
+                    if(sequence[i][j-1]=='@') count++;
+                }else if(i-1>=0){
+                    if(sequence[i-1][j]=='@') count++;
+                }else if(j-1>=0){
+                    if(sequence[i][j-1]=='@') count++;
+                }
+                if(i+1<sequence.size()&&j+1<sequence[i].size()){
+                    if(sequence[i+1][j+1]=='@') count++;
+                    if(sequence[i+1][j]=='@') count++;
+                    if(sequence[i][j+1]=='@') count++;
+                }else if(i+1<sequence.size()){
+                    if(sequence[i+1][j]=='@') count++;
+                }else if(j+1<sequence[i].size()){
+                    if(sequence[i][j+1]=='@') count++;  
+                }
+                if(i-1>=0&&j+1<sequence[i].size()){
+                    if(sequence[i-1][j+1]=='@') count++;
+                }
+                if(i+1<sequence.size()&&j-1>=0){
+                    if(sequence[i+1][j-1]=='@') count++;
+                }
+                if(count<4){
+                    sum++;
+                    sequence[i][j] = '.';
+                    //std::cout<<"Position "<<i<<","<<j<<" is weak with "<<count<<" supports."<<std::endl;
+                    change = true;
+                }
+            }
+        }
+    }
+    return sum;
+}
+int main() {
+    std::vector<std::string> sequence;
+    std::string s;
+    while (std::cin >> s) {
+        sequence.push_back(s);
+    }
+    std::cout <<"The sum is: "<< forklifting(sequence) <<" !" << std::endl;
+    return 0;
+}
